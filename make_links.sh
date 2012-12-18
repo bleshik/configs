46044ddebab646b9a83913eb3ff9bbfefe0f7634
Alexey Balchunas <bleshik@gmail.com>
2012-12-18 18:44:20 +0000

    make_links script fix

--- make_links.sh
+++ make_links.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ln -sF $PWD/.vim ~/.vim
-ln -sF $PWD/.vimrc ~/.vimrc
-ln -sF $PWD/.bashrc ~/.bashrc
-ln -sF $PWD/.bash_profile ~/.bash_profile
-ln -sF $PWD/.tmux.conf ~/.tmux.conf
-ln -sF $PWD/.gitconfig ~/.gitconfig
+ln -sFf $PWD/.vim ~/.vim
+ln -sFf $PWD/.vimrc ~/.vimrc
+ln -sFf $PWD/.bashrc ~/.bashrc
+ln -sFf $PWD/.bash_profile ~/.bash_profile
+ln -sFf $PWD/.tmux.conf ~/.tmux.conf
+ln -sFf $PWD/.gitconfig ~/.gitconfig
