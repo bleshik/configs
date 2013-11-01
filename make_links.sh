b64f2ee8e7fb7437d8ef466d1c4b0ac909a267e3
Alexey Balchunas <bleshik@yandex-team.ru>
2013-11-01 10:54:14 +0400

    added aliases

--- make_links.sh
+++ make_links.sh
@@ -3,6 +3,7 @@
 ln -sFf $PWD/.vim ~/.vim
 ln -sFf $PWD/.vimrc ~/.vimrc
 ln -sFf $PWD/.bashrc ~/.bashrc
+ln -sFf $PWD/.bash_aliases ~/.bash_aliases
 ln -sFf $PWD/.bash_profile ~/.bash_profile
 ln -sFf $PWD/.tmux.conf ~/.tmux.conf
 ln -sFf $PWD/.gitconfig ~/.gitconfig
